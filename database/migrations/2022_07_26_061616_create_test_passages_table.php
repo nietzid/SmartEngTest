b54018d110d58cd1287ba37070e5d85ba50c62d3
nietzid <arfiansyah@upi.edu>
2022-07-26 21:38:18 +0700

    Update Migration and Seeder

--- database/migrations/2022_07_26_061616_create_test_passages_table.php
+++ database/migrations/2022_07_26_061616_create_test_passages_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('passages', function (Blueprint $table) {
             $table->id();
-            $table->string("passages");
+            $table->longText("passages");
         });
     }
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Passage;
 use App\Models\Question;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,11 +17,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//         \App\Models\User::factory(10)->create();
-
-         Question::factory(20)->create([
-             'name' => 'Test User',
-             'email' => 'test@example.com',
-         ]);
+        User::factory(20)->create();
+        Passage::factory(20)->create();
+        Question::factory(20)->create();
     }
 }
